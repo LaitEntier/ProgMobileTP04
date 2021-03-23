7c7acfb43be9d4b4b009265094c720425d1f2302
LaitEntier <axelzinc@hotmail.fr>
2021-03-23 19:59:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tp04/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.tp04;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.tp04.ui.main.SectionsPagerAdapter;
+
+import java.util.Locale;
+
+import static com.google.android.material.snackbar.Snackbar.make;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        Snackbar snackBar = Snackbar.make(getWindow().getDecorView().findViewById(android.R.id.content),
+                "Look at me, I'm a fancy snackbar", Snackbar.LENGTH_LONG);
+        snackBar.show();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Créer un adaptateur qui retourne un fragment pour chacune des sections de l'activité
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        // dire au viewpager d'utiliser cet adaptateur
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+   /* bouton flottant
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tp04/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,80 @@
+package com.example.tp04.ui.main;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.tp04.NatureFragment;
+import com.example.tp04.R;
+
+import java.util.Locale;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+
+    public int getCount() {
+        // Nombre de pages à considérer.
+        return 3;
+    }
+
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return NatureFragment.newInstance(0, mContext.getString(R.string.titre_section0));
+            case 1:
+                return NatureFragment.newInstance(1, mContext.getString(R.string.titre_section1));
+            case 2:
+                return NatureFragment.newInstance(2, mContext.getString(R.string.titre_section2));
+        }
+        return null;
+    }
+
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        String titre="";
+        Drawable icon=null;
+        switch (position) {
+            case 0:
+                titre = mContext.getString(R.string.titre_section0).toUpperCase(l);
+                icon = mContext.getResources().getDrawable(R.drawable.ic_mineral);
+                break;
+            case 1:
+                titre = mContext.getString(R.string.titre_section1).toUpperCase(l);
+                icon = mContext.getResources().getDrawable(R.drawable.ic_vegetal);
+                break;
+            case 2:
+                titre = mContext.getString(R.string.titre_section2).toUpperCase(l);
+                icon = mContext.getResources().getDrawable(R.drawable.ic_animal);
+                break;
+        }
+        SpannableString sb = new SpannableString(" " + titre);
+        // un espace est ajouté pour séparer le texte de l'image
+
+        icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
+        ImageSpan span = new ImageSpan(icon, ImageSpan.ALIGN_BASELINE);
+        sb.setSpan(span, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return sb;
+    }
+}
